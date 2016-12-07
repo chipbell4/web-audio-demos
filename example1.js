7742132a48679b51373964c26d106fb8a0760139
Chip Bell <cbell@pbs.org>
2016-12-07 13:20:00 -0500

    First example

---
+++ example1.js
@@ -0,0 +1,27 @@
+function example1(context) {
+  var oscillator = context.createOscillator();
+  var gain = context.createGain();
+
+  // set up the oscillator
+  oscillator.type = 'square';
+  oscillator.frequency.value = 220;
+
+  // set the volume
+  gain.gain.value = 0.1;
+
+  // connect nodes
+  oscillator.connect(gain);
+  gain.connect(context.destination);
+
+  // start the oscillator
+  oscillator.start();
+
+  // toggle gain to make it beep
+  setInterval(function() {
+    if(gain.gain.value > 0) {
+      gain.gain.value = 0;
+    } else {
+      gain.gain.value = 0.1;
+    }
+  }, 250);
+}
