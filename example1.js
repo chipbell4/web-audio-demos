7dea2ef8128d66e5639f579d74620d747939b83f
Chip Bell <cbell@pbs.org>
2016-12-07 13:34:47 -0500

    Example 1, playing audio

---
+++ example1.js
@@ -0,0 +1,29 @@
+function example1(context) {
+  // load the audio via ajax
+  var request = new XMLHttpRequest();
+  request.open('GET', 'cave.mp3', true);
+  request.responseType = 'arraybuffer';
+  request.onload = onLoad;
+  request.send();
+
+  function onLoad() {
+    context.decodeAudioData(request.response, audioDecoded);
+  }
+
+  function audioDecoded(buffer) {
+    // set up the buffer for storing the audio data
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+  
+    // set the volume
+    var gain = context.createGain();
+    gain.gain.value = 0.4;
+  
+    // connect nodes
+    source.connect(gain);
+    gain.connect(context.destination);
+
+    // start playing
+    source.start(0);
+  }
+}
