2928813bc2c00ba9b6c668ab86f5d5a8f49cd7f7
Chip Bell <cbell@pbs.org>
2016-12-07 14:56:50 -0500

    Distortion example

---
+++ example6.js
@@ -0,0 +1,42 @@
+function example6(context) {
+  // load the audio via ajax
+  var request = new XMLHttpRequest();
+  request.open('GET', 'cave.mp3', true);
+  request.responseType = 'arraybuffer';
+  request.onload = onLoad;
+  request.send();
+
+  function onLoad() {
+    context.decodeAudioData(request.response, audioDecoded);
+  }
+
+  function audioDecoded(buffer) {
+    // set up the buffer for storing the audio data
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+
+    // create a distortion node (with a corresponding curve)
+    var curve = new Float32Array(44100);
+    var alpha = 1;
+    var beta = 50;
+    for(var i = 0; i < 44100; i++) {
+      var x = (i / 44100) * 2 - 1; // scale i to be between -1 and 1;
+      curve[i] = (alpha + beta) * x / (alpha + beta * Math.abs(x)); // sort of a "logistic" curve
+    }
+    var distortion = context.createWaveShaper();
+    distortion.curve = curve;
+    distortion.oversample = '4x';
+  
+    // set the volume
+    var gain = context.createGain();
+    gain.gain.value = 0.2;
+  
+    // connect nodes
+    source.connect(distortion);
+    distortion.connect(gain);
+    gain.connect(context.destination);
+
+    // start playing
+    source.start(0);
+  }
+}
